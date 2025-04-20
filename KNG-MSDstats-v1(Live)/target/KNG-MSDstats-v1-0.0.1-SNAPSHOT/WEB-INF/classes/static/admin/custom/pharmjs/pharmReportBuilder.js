2df70f04e41708994208f8faff354d665868e2f5
LUK34 <lukerajan.mathew@gmail.com>
2025-04-20 09:13:14 +0300

    Commited on 20 April 2025 at 09:14 AM

--- KNG-MSDstats-v1(Live)/target/KNG-MSDstats-v1-0.0.1-SNAPSHOT/WEB-INF/classes/static/admin/custom/pharmjs/pharmReportBuilder.js
+++
@@ -1,9 +0,0 @@
-import {coreReportModuleWiseBuilder} from '../brtjs/coreReportModuleWiseBuilder.js';
-
-document.addEventListener("DOMContentLoaded", function() 
-{
-	const lang = new URLSearchParams(window.location.search).get('lang');
-  	console.log("Language value is: " + lang);
-    coreReportModuleWiseBuilder("dynamicPharmReportContainer", "Pharmacy", "الصيدلية",lang);
-});
-			
